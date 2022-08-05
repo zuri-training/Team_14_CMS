d3f997afd63ed8cb0e291f9d87d39acca198dd82
Dilibe Fidelugwuowo <franklinfidelugwuowo@gmail.com>
2022-08-05 12:55:02 +0100

    [Feature]: Linked the signin page

--- cmsproject/cmsproject/settings.py
+++ cmsproject/cmsproject/settings.py
@@ -137,7 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'landingpage'
 
-LOGOUT_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'landingpage'
 
 # Connecting the base url to the media folder
 
