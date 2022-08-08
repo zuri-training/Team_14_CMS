bcfdc5a0506d5f1d606e5a76e68976ac6201e3d2
Dilibe Fidelugwuowo <franklinfidelugwuowo@gmail.com>
2022-08-08 14:54:26 +0100

    [Fix]: Resolved url mapping error

--- cmsapp/models.py
+++ cmsapp/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
-from account.models import User
+from accounts.models import User
+from datetime import datetime
 
 # Create your models here.
 class Post (models.Model):
@@ -17,19 +18,19 @@ class Post (models.Model):
     post_id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
 
     # creation date
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True,)
 
     # modified date
-    modified_at = models.DateTimeField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now=True,)
 
     # slug field 
     slug = models.SlugField()
     
     # associating a post to a user
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
 
     # to upload images 
-    photo = models.ImageField()
+    photo = models.ImageField(upload_to='Picture_Images', default='image.jpg')
 
     def __str__(self):
         return self.title
